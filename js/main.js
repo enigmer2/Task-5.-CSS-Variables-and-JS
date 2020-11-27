2fd27aecf7d1fb710695c0a7d94d3624a1b431b3
Dzmitry Datskevich <dda.enigmer@gmail.com>
2020-11-27 20:22:58 +0300

    fix: issue with h1 & class name

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const inputs = document.querySelectorAll('.controls input');
+const inputs = document.querySelectorAll('.form-controls input');
 
 function handleUpdate() {
   const suffix = this.dataset.sizing || '';
